a5538d3cf798f23393d173c194eb1d79d7b375ff
hristo-vrigazov <hvrigazov@gmail.com>
2016-12-03 13:40:15 +0200

    Added signs mapping and visualization of a training example

--- train/describe_dataset.py
+++ train/describe_dataset.py
@@ -1,4 +1,5 @@
 from load_dataset import load_dataset
+import cv2
 
 X_train, y_train, X_test, y_test = load_dataset()
 
@@ -14,3 +15,6 @@ print("Number of training examples =", n_train)
 print("Number of testing examples =", n_test)
 print("Image data shape =", image_shape)
 print("Number of classes =", n_classes)
+
+cv2.imshow('Training example', X_train[5000])
+cv2.waitKey(0)
