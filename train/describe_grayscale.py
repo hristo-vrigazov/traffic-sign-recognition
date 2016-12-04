e0419c387bdecab36a85e4f6092dfd5769acbada
hristo-vrigazov <hvrigazov@gmail.com>
2016-12-04 14:04:09 +0200

    Help garbage collection by clearing the pickle from memory; started implementing grayscale transforming to the dataset

---
+++ train/describe_grayscale.py
@@ -0,0 +1,20 @@
+from load_dataset import load_grayscale
+import cv2
+
+X_train, y_train, X_test, y_test = load_grayscale()
+
+n_train = len(X_train)
+
+n_test = len(X_test)
+
+image_shape = X_train[0].shape
+
+n_classes = len(set(y_train))
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+cv2.imshow('Training example', X_train[5000])
+cv2.waitKey(0)
--- train/load_dataset.py
+++ train/load_dataset.py
@@ -3,8 +3,10 @@ import pickle
 import os.path
 import os
 import wget
-
+import cv2
 import zipfile
+import numpy as np
+
 
 def unzip(source_filename, dest_dir):
     with zipfile.ZipFile(source_filename) as zf:
@@ -24,22 +26,38 @@ def unzip(source_filename, dest_dir):
                 path = os.path.join(path, word)
             zf.extract(member, path)
 
+
+def load_grayscale():
+    X_train, y_train, X_test, y_test = load_dataset()
+
+    for i in range(len(X_train)):
+        X_train[i] = cv2.cvtColor(X_train[i], cv2.COLOR_BGR2GRAY)
+
+    for i in range(len(X_test)):
+        X_test[i] = cv2.cvtColor(X_test[i], cv2.COLOR_BGR2GRAY)
+
+    return X_train, y_train, X_test, y_test
+
+
 def load_dataset():
-	training_file = 'train.p'
-	testing_file = 'test.p'
+    training_file = 'train.p'
+    testing_file = 'test.p'
 
-	if not os.path.isfile(training_file) or not os.path.isfile(testing_file):
-		url = 'https://d17h27t6h515a5.cloudfront.net/topher/2016/November/581faac4_traffic-signs-data/traffic-signs-data.zip'
-		filename = wget.download(url)
-		unzip(filename, '.')
-		os.remove(filename)
+    if not os.path.isfile(training_file) or not os.path.isfile(testing_file):
+        url = 'https://d17h27t6h515a5.cloudfront.net/topher/2016/November/581faac4_traffic-signs-data/traffic-signs-data.zip'
+        filename = wget.download(url)
+        unzip(filename, '.')
+        os.remove(filename)
 
-	with open(training_file, mode='rb') as f:
-	    train = pickle.load(f)
-	with open(testing_file, mode='rb') as f:
-	    test = pickle.load(f)
+    with open(training_file, mode='rb') as f:
+        train = pickle.load(f)
+    with open(testing_file, mode='rb') as f:
+        test = pickle.load(f)
 
-	X_train, y_train = train['features'], train['labels']
-	X_test, y_test = test['features'], test['labels']
+    X_train, y_train = train['features'], train['labels']
+    X_test, y_test = test['features'], test['labels']
+    
+    del train
+    del test
 
-	return X_train, y_train, X_test, y_test
+    return X_train, y_train, X_test, y_test
